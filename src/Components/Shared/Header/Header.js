3f5178364e88143887b6c0028c7280223c04de1a
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-08-26 08:00:44 +0600

    header nevigation panel done

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './Header.css'
 const Header = () => {
     return (
-        <div>
-            <h1 className='title'><Link to="/">Funshion</Link></h1>
-            <div className='navigation-bar'>
-      <ul className='font-thin'>
-       <li><Link to="/home">Home</Link> </li>
-      <li><Link to="/about">About</Link></li>
-    <li><Link to="/shop">Shop</Link></li>
-    <li><Link to="/pages">Pages</Link></li>
-    <li><Link to="blog">Blog</Link></li>
-    
+        <div className='h-[75px] w-full flex'>
+            <div className='flex lg:w-[80%] mx-auto justify-between items-center'>
+            <h1 className='title text-3xl'><Link to="/">Funshion</Link></h1>
+            <div className='navigation-bar '>
+      <ul className='font-thin flex items-center'>
+    <li className='mx-2'><Link to="/home">Home</Link> </li>
+    <li className='mx-2'><Link to="/about">About</Link></li>
+    <li className='mx-2'><Link to="/shop">Shop</Link></li>
+    <li className='mx-2'><Link to="/pages">Pages</Link></li>
+    <li className='mx-2'><Link to="blog">Blog</Link></li> 
     </ul>
+    <ul className='icon'>
+
+
+    </ul>
+            </div>
             </div>
         </div>
     );
