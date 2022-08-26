14de2ca2bf081491d15554ec817c76667b10155c
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-08-26 13:04:14 +0600

    special benefit added

--- src/Components/Pages/HomePage/Home/Home.js
+++ src/Components/Pages/HomePage/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import Specialbenefit from "../SpecialBenefit/Specialbenefit";
 
 const Home = () => {
   return (
     <div className="w-full">
       <Banner></Banner>
+      <Specialbenefit></Specialbenefit>
     </div>
   );
 };
---
+++ src/Components/Pages/HomePage/SpecialBenefit/Specialbenefit.js
@@ -0,0 +1,38 @@
+import React from "react";
+import delivery from "./../../../../Images/benefit/tracking 1 (1).png";
+const Specialbenefit = () => {
+  return (
+    <div className="lg:w-[85%] justify-between grid md:grid-cols-2 w-full  grid-cols-1  gap-5 lg:grid-cols-4 mx-auto">
+      <div className="flex mx-auto lg:mx-0 items-center gap-3 ">
+        <img src={delivery} alt="" />
+        <div>
+          <h1 className="text-[14px]">Fast Delivery</h1>
+          <p className="text-[12px]  ">Lorem Ipsum is simply dummy text </p>
+        </div>
+      </div>
+      <div className="flex  mx-auto lg:mx-0 items-center gap-3 ">
+        <img src={delivery} alt="" />
+        <div>
+          <h1 className="text-[14px]">Fast Delivery</h1>
+          <p className="text-[12px]  ">Lorem Ipsum is simply dummy text </p>
+        </div>
+      </div>
+      <div className="flex mx-auto lg:mx-0 items-center gap-3 ">
+        <img src={delivery} alt="" />
+        <div>
+          <h1 className="text-[14px]">Fast Delivery</h1>
+          <p className="text-[12px]  ">Lorem Ipsum is simply dummy text </p>
+        </div>
+      </div>
+      <div className="flex mx-auto lg:mx-0 items-center gap-3 ">
+        <img src={delivery} alt="" />
+        <div>
+          <h1 className="text-[14px]">Fast Delivery</h1>
+          <p className="text-[12px]  ">Lorem Ipsum is simply dummy text </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Specialbenefit;
