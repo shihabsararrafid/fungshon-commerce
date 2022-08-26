0c90f6ec810bb597976e1fc39a09d47b973d7265
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-08-26 12:36:15 +0600

    banner portion done

--- src/Components/Pages/HomePage/Banner/Banner.js
+++ src/Components/Pages/HomePage/Banner/Banner.js
@@ -5,10 +5,10 @@ import bannersmall from "./../../../../Images/Banner/bannersm.png";
 import "./Banner.css";
 const Banner = () => {
   return (
-    <div className="lg:w-[85%] banner-container md:w-[95%] w-full mx-auto my-20 justify-between flex flex-col lg:flex-row  ">
+    <div className="lg:w-[85%] banner-container md:w-[95%] w-full mx-auto my-20 lg:justify-between flex flex-col lg:flex-row  ">
       <div className="lg:w-[40%] w-full text-justify">
         {" "}
-        <h1 className="text-6xl my-6">Top Designers Pick</h1>
+        <h1 className="md:text-6xl text-3xl  my-6">Top Designers Pick</h1>
         <p className="mb-[36px]">
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. The point of
@@ -17,8 +17,12 @@ const Banner = () => {
         <button className="block mx-auto lg:mx-0 mb-20 btn">Shop Now</button>
         <Counter></Counter>
       </div>
-      <div className=" relative   mx-auto ">
-        <img className="relative top-0 left-0" src={bannerimg} alt="" />
+      <div className=" lg:relative my-10 lg:my-0   px-auto ">
+        <img
+          className="lg:relative w-full  top-0 left-0"
+          src={bannerimg}
+          alt=""
+        />
 
         <img
           className="absolute -left-[170px] hidden lg:block top-[90px]"
