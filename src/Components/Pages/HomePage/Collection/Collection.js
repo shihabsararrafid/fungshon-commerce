964419b9023ba44a47260cf132610efe51d275ed
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-08-26 16:09:58 +0600

    sale part done

--- src/Components/Pages/HomePage/Collection/Collection.js
+++ src/Components/Pages/HomePage/Collection/Collection.js
@@ -2,20 +2,30 @@ import React from "react";
 import sale from "./../../../../Images/collection/sale (1).png";
 import men from "./../../../../Images/collection/men.png";
 import women from "./../../../../Images/collection/women.png";
+import "./Collection.css";
 const Collection = () => {
   return (
     <div className="my-20 lg:w-[85%] grid grid-cols-3 mx-auto">
       <div className="img-container w-[80%] relative">
         <img className="w-full" src={sale} alt="" />
-        <div className="absolute top-[5%] left-[5%]  w-[90%] h-[90%] border-[5px] bg-transparent border-white"></div>
-      </div>
-      <div className="img-container w-[80%] relative">
-        <img className="w-full" src={men} alt="" />
-        <div className="absolute top-[5%] left-[5%]  w-[90%] h-[90%] border-[5px] bg-transparent border-white"></div>
+        <div className="box ">
+          <h1 className="absolute bottom-[15%] left-[9%] text-4xl">
+            Up to <span className="bg-[#F09522] py-1 px-3">50%</span>
+            Off Sale{" "}
+          </h1>
+          <p className="check-btn">
+            Check now
+            <span className="line"></span>
+          </p>
+        </div>
       </div>
       <div className="img-container w-[80%] relative">
         <img className="w-full" src={women} alt="" />
-        <div className="absolute top-[5%] left-[5%]  w-[90%] h-[90%] border-[5px] bg-transparent border-white"></div>
+        <div className="box"></div>
+      </div>
+      <div className="img-container w-[80%] relative">
+        <img className="w-full" src={men} alt="" />
+        <div className="box"></div>
       </div>
     </div>
   );
