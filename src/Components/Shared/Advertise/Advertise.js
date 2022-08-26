7d72c1546e9c5bc83d909c5147ea082e334b6389
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-08-26 07:28:37 +0600

    advertise component responsive done

--- src/Components/Shared/Advertise/Advertise.js
+++ src/Components/Shared/Advertise/Advertise.js
@@ -2,10 +2,11 @@ import React from 'react';
 import fbicon from './../../../Images/Icons/Facebook Icon.png';
 import instaicon from './../../../Images/Icons/Instagram.png';
 import pintericon from './../../../Images/Icons/Pinterest.png';
+import './Advertise.css';
 const Advertise = () => {
     return (
         <div className='bg-black flex  items-center h-[75px] w-full text-white'>
-          <div className='flex justify-between lg:w-[85%] mx-auto'>
+          <div className='flex small-container justify-between w-[95%] lg:w-[85%] mx-auto'>
           <p>Free shipping on all UK orders!</p> 
           <div className="icons flex ">
             <img className='block mx-3 cursor-pointer' src={fbicon} alt="" />
