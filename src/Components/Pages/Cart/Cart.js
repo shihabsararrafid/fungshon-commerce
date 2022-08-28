237e7389a17ae74c65cb4c13da74f331e45572b9
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-08-28 10:22:29 +0600

    cart details updated

--- src/Components/Pages/Cart/Cart.js
+++ src/Components/Pages/Cart/Cart.js
@@ -1,16 +1,39 @@
 import React, { useContext } from "react";
 import { useCartId } from "../../../App";
 import SingleCartElement from "./SingleCartElement";
-
+import "./Cart.css";
 const Cart = () => {
   const arrId = useContext(useCartId);
   return (
     <div className="w-[85%] font-serif mx-auto">
       <h1 className="text-3xl ">Shopping Cart</h1>
-      {arrId.length}
-      {arrId.map((i) => (
-        <SingleCartElement id={i}></SingleCartElement>
-      ))}
+      {arrId?.length === 0 ? (
+        <h1 className="text-3xl text-center ">You have no items in cart</h1>
+      ) : (
+        ""
+      )}
+      {arrId?.length > 0 ? (
+        <table id="cart" className="w-full ">
+          <tr>
+            <th className="py-[12px] text-left bg-black text-white">Image</th>
+            <th className="py-[12px] text-left bg-black text-white">
+              Product Name
+            </th>
+            <th className="py-[12px] text-left bg-black text-white">
+              Quantity
+            </th>
+            <th className="py-[12px] text-left bg-black text-white">
+              Unit Price
+            </th>
+            <th className="py-[12px] text-left bg-black text-white">Total</th>
+          </tr>
+          {arrId.map((i) => (
+            <SingleCartElement id={i}></SingleCartElement>
+          ))}
+        </table>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
--- src/Components/Pages/Cart/SingleCartElement.js
+++ src/Components/Pages/Cart/SingleCartElement.js
@@ -3,16 +3,69 @@ import React, { useEffect, useState } from "react";
 const SingleCartElement = ({ id }) => {
   const [product, setProduct] = useState([]);
   console.log(product, id);
+  //const [total, setTotal] = useState(0);
+  const [value, setValue] = useState(1);
   useEffect(() => {
     fetch(`https://fakestoreapi.com/products/${id}`)
       .then((res) => res.json())
-      .then((data) => setProduct(data));
+      .then((data) => {
+        setProduct(data);
+        // setTotal(data?.price);
+      });
   }, [id]);
+  const input = document.getElementById("input");
+  const plus = document.getElementById("plus");
+  const minus = document.getElementById("minus");
+  const total = document.getElementById("total");
+  // console.log(total?.innerText);
+  let totalPrice = product?.price;
+  const minusClicked = () => {
+    let prim = parseInt(input.value);
+    input.value = input.value > 0 ? prim - 1 : 0;
+    totalPrice = parseFloat(product?.price) * parseInt(input?.value);
+    console.log(totalPrice);
+    console.log(total.innerText);
+    total.innerText = `${totalPrice}`;
+    //setTotal(totalPrice);
+  };
+
+  const plusClicked = () => {
+    let prim = parseInt(input?.value);
+
+    input.value = prim + 1;
+    totalPrice = parseFloat(product?.price) * parseInt(input?.value);
+    console.log(totalPrice);
+    total.innerText = `$${totalPrice}`;
+  };
+
   return (
-    <div>
-      <p>Item</p>
-      <p>{product?.title}</p>
-    </div>
+    <tr>
+      <td>
+        <img
+          className="w-[50px] h-[50px] object-fill"
+          src={product?.image}
+          alt=""
+          srcset=""
+        />
+      </td>
+      <td>{product?.title?.slice(0, 30)}</td>
+      <td>
+        <span onClick={minusClicked} id="minus " className="cursor-pointer">
+          -
+        </span>{" "}
+        <input
+          id="input"
+          className="w-[30%] text-center"
+          type="text"
+          value="1"
+        />{" "}
+        <span onClick={plusClicked} id="plus" className="cursor-pointer">
+          +
+        </span>
+      </td>
+      <td>${product?.price}</td>
+      <td id="total">${totalPrice}</td>
+    </tr>
   );
 };
 
